ef08a60d431d96f9e6f337983b41118b48ccd5df
Alibek Manabayev <alibek.manabayev@gmail.com>
2024-08-09 14:05:54 +0500

    gorg: reorganize photos into YYYY/YYYY-MM-DD/ID.CR2 format

--- pkg/gorg/organize.go
+++ pkg/gorg/organize.go
@@ -5,7 +5,6 @@ import (
 	"log/slog"
 	"os"
 	"path/filepath"
-	"strings"
 
 	"github.com/m-nny/gorg/pkg/metadata"
 )
@@ -13,51 +12,57 @@ import (
 const SUBFOLDER_FORMAT = "2006/2006-01-02"
 const NEW_PERM os.FileMode = 0755
 
-func NewFilename(folder string) (int, error) {
+func isFolderEmpty(folder string) (bool, error) {
 	f, err := os.Open(folder)
 	if err != nil {
-		return 0, err
+		return false, err
 	}
 	files, err := f.Readdirnames(-1)
 	if err != nil {
-		return 0, err
+		return false, err
 	}
-	cnt := 0
-	for _, file := range files {
-		if strings.HasSuffix(file, metadata.METADATA_EXT) {
-			continue
-		}
-		cnt++
-	}
-	return cnt, nil
+	return len(files) == 0, nil
 }
 
-func Organize(targetFolder string, meta *metadata.Metadata) error {
-	newFolder := filepath.Join(targetFolder, meta.CreatedAt.Format(SUBFOLDER_FORMAT))
-	if err := os.MkdirAll(newFolder, NEW_PERM); err != nil {
-		return err
-	}
-	newFolderFileCnt, err := NewFilename(newFolder)
-	if err != nil {
-		return err
-	}
-	newFilename := fmt.Sprintf("%04d%s", newFolderFileCnt, filepath.Ext(meta.FullFilepath))
-	newFilepath := filepath.Join(newFolder, newFilename)
-	slog.Debug("Organize()", "newFilename", newFilename, "newFolder", newFolder, "newFilepath", newFilepath, "meta", meta)
-	if err := os.Link(meta.FullFilepath, newFilepath); err != nil {
-		return err
-	}
-	if err := meta.Copy(newFilepath).Save(); err != nil {
-		return err
+type MetaMap map[string][]*metadata.Metadata
+
+func (m MetaMap) Add(meta *metadata.Metadata) {
+	newFolder := meta.CreatedAt.Format(SUBFOLDER_FORMAT)
+	m[newFolder] = append(m[newFolder], meta)
+}
+
+func (m MetaMap) organize(destFolder string) error {
+	for subFolder, metas := range m {
+		newFolder := filepath.Join(destFolder, subFolder)
+		if err := os.MkdirAll(newFolder, NEW_PERM); err != nil {
+			return err
+		}
+		isEmpty, err := isFolderEmpty(newFolder)
+		if err != nil {
+			return err
+		}
+		if !isEmpty {
+			return fmt.Errorf("subfolder %s is not empty", newFolder)
+		}
+		for i, meta := range metas {
+			newFilename := fmt.Sprintf("%04d%s", i, filepath.Ext(meta.FullFilepath))
+			newFilepath := filepath.Join(newFolder, newFilename)
+			slog.Debug("Organize()", "newFilename", newFilename, "newFolder", newFolder, "newFilepath", newFilepath, "meta", meta)
+			if err := os.Link(meta.FullFilepath, newFilepath); err != nil {
+				return err
+			}
+			if err := meta.Copy(newFilepath).Save(); err != nil {
+				return err
+			}
+		}
 	}
 	return nil
 }
 
 func BulkOrganize(targetFolder string, metas []*metadata.Metadata) error {
+	m := MetaMap{}
 	for _, meta := range metas {
-		if err := Organize(targetFolder, meta); err != nil {
-			return err
-		}
+		m.Add(meta)
 	}
-	return nil
+	return m.organize(targetFolder)
 }
